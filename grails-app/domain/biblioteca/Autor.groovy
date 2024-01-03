c8a1dd9122094efd8f44e6aaf9bc84aeac763b55
Allan Ferreira <allansergio1@gmail.com>
2024-01-03 15:47:42 -0300

    Criação de controllers e views

--- grails-app/domain/biblioteca/Autor.groovy
+++ grails-app/domain/biblioteca/Autor.groovy
@@ -12,7 +12,7 @@ class Autor {
     // Restrições de validação
     static constraints = {
         nome blank: false, unique: true // O nome não pode ser vazio nem repetido
-        nacionalidade inList: ['Brasil', 'Portugal', 'Angola', 'Moçambique', 'Cabo Verde', 'Guiné-Bissau', 'São Tomé e Príncipe', 'Timor-Leste'] // A nacionalidade deve ser um dos países de língua portuguesa
+        nacionalidade blank: false
         dataNascimento nullable: true, max: new Date() // A data de nascimento pode ser nula, mas não pode ser maior que a data atual
     }
 }
